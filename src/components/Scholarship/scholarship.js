7f81e0079f58650b59708e2b1efccef44c249c12
ammar-tanwar <ammartanwar123@gmail.com>
2024-03-15 16:31:54 +0530

    new page marketing and email

--- src/components/Scholarship/scholarship.js
+++ src/components/Scholarship/scholarship.js
@@ -3,37 +3,55 @@ import React from "react";
 import FormOtp from "../Global/Form/Form";
 import Styles from "./scholarship.module.css";
 
-function Scholarship({ Offer }) {
+function Scholarship({ Offer, reactEmail, reactPhone }) {
   return (
     <div className={Styles.container}>
       <div className={Styles.image}>
-      <Image
-            src="https://d32and0ii3b8oy.cloudfront.net/web/s3_main/learnbayMain/popup-left.webp"
-            width={400}
-            height={470}
-            alt="Scholarship"
-          />
+        <Image
+          src="https://d32and0ii3b8oy.cloudfront.net/web/s3_main/learnbayMain/popup-left.webp"
+          width={400}
+          height={470}
+          alt="Scholarship"
+        />
       </div>
       <div className={Styles.FormDiv}>
-        {Offer ? (
+        {reactPhone ? (
           <>
-            <h4>Apply for Offer</h4>
-            <FormOtp
-              upSkillingHide={true}
-              s2dataScience={true}
-              HideInterest={true}
-              promoCode={true}
-            />
-          </>
+          <h4>Apply for Scholarship</h4>
+          <FormOtp dataScience={true} emailHidden={true} />
+        </>
         ) : (
           <>
-            <h4>Apply for Scholarship</h4>
-            <FormOtp
-              upSkillingHide={true}
-              CTC={true}
-              s2dataScience={true}
-              HideInterest={true}
-            />
+            {reactEmail ? (
+              <>
+                <h4>Apply for Scholarship</h4>
+                <FormOtp dataScience={true} PhoneHidden={true} />
+              </>
+            ) : (
+              <>
+                {Offer ? (
+                  <>
+                    <h4>Apply for Offer</h4>
+                    <FormOtp
+                      upSkillingHide={true}
+                      s2dataScience={true}
+                      HideInterest={true}
+                      promoCode={true}
+                    />
+                  </>
+                ) : (
+                  <>
+                    <h4>Apply for Scholarship</h4>
+                    <FormOtp
+                      upSkillingHide={true}
+                      CTC={true}
+                      s2dataScience={true}
+                      HideInterest={true}
+                    />
+                  </>
+                )}
+              </>
+            )}
           </>
         )}
       </div>
