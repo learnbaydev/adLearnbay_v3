e0c7f526f57990300dd4a7185e1e08a0c61b8633
ammar-tanwar <ammartanwar123@gmail.com>
2024-06-11 17:45:44 +0530

    change batch details

--- src/components/CoursePage/FeeSection/FeeContent.js
+++ src/components/CoursePage/FeeSection/FeeContent.js
@@ -168,14 +168,12 @@ const FeeContent = ({
           <div className={styles.third}>
             <p>Batch Details</p>
             <div className={styles.boxOrange}>
-              <div className={styles.batchImg}>
-                <p>{weekendbatch}</p>
-              </div>
+              <p> {weekdaybatch}</p>
               <div className={styles.batches}>
-                <p className={styles.date}>{WeekendDate}</p>
+                <p className={styles.date}>{WeekdayDate}</p>
                 <div className={styles.Time}>
-                  <p className={styles.day}>{weekend}</p>
-                  <p className={styles.paraBot}>{WeekendTime}</p>
+                  <p className={styles.day}>{weekday}</p>
+                  <p className={styles.paraBot}>{WeekdayTime}</p>
                 </div>
               </div>
             </div>
@@ -187,12 +185,14 @@ const FeeContent = ({
               }}
             />
             <div className={styles.boxOrange}>
-              <p> {weekdaybatch}</p>
+              <div className={styles.batchImg}>
+                <p>{weekendbatch}</p>
+              </div>
               <div className={styles.batches}>
-                <p className={styles.date}>{WeekdayDate}</p>
+                <p className={styles.date}>{WeekendDate}</p>
                 <div className={styles.Time}>
-                  <p className={styles.day}>{weekday}</p>
-                  <p className={styles.paraBot}>{WeekdayTime}</p>
+                  <p className={styles.day}>{weekend}</p>
+                  <p className={styles.paraBot}>{WeekendTime}</p>
                 </div>
               </div>
             </div>
