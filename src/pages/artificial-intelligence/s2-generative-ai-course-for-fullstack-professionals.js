d2441d274221f5ccd12fc17d40d69a0b33f56566
sohel <syed.sohel@learnbay.co>
2025-07-25 18:17:47 +0530

    updated the course fee with emi options

--- src/pages/artificial-intelligence/s2-generative-ai-course-for-fullstack-professionals.js
+++ src/pages/artificial-intelligence/s2-generative-ai-course-for-fullstack-professionals.js
@@ -25,62 +25,62 @@ import AnimationGEN from "@/components/CoursePage/AnimationDsa/AnimationGEN";
 import GenAIProject from "@/components/CoursePage/genAiProject/GenAIProject";
 
 function Blockchain({ DataScienceCourseDataJson }) {
-  
-    const JobData = {
-        title: "Program outcome: What’s in it for you?",
-        points: [
-          {
-            imgSrc:
-              "https://d32and0ii3b8oy.cloudfront.net/web/s3_main/Course-home/adobe_illustrator+(1).webp",
-            imgAlt: "",
-            title: "Master GenAI Tools + Frameworks",
-            description:
-              "Learn OpenAI APIs, LangChain, Hugging Face, and more to build intelligent applications effortlessly.",
-          },
-          {
-            imgSrc:
-              "https://d32and0ii3b8oy.cloudfront.net/web/s3_main/Course-home/generated_photos+(1).webp",
-            imgAlt: "",
-            title:
-              "Real-World GenAI Project Portfolio",
-            description:
-              "Work on live projects like chatbot creation, workflow automation, and RAG-powered assistants to showcase your expertise.",
-          },
-          {
-            imgSrc:
-              "https://d32and0ii3b8oy.cloudfront.net/web/s3_main/Course-home/smart_upscaler+(1).webp",
-            imgAlt: "",
-            title: "Higher Earning Potential",
-            description:
-              "Leverage advanced GenAI knowledge to drive business success, enhancing career prospects and salary growth.",
-          },
-        ],
-        bannerImage: {
-          src: "https://d32and0ii3b8oy.cloudfront.net/web/s3_main/Course-home/genAI_DEV+(1).webp  ",
-          width: 350,
-          height: 350,
-          priority: true,
-          quality: 80,
-          alt: "Banner Image 1 Alt",
-        },
-      };
+  const JobData = {
+    title: "Program outcome: What’s in it for you?",
+    points: [
+      {
+        imgSrc:
+          "https://d32and0ii3b8oy.cloudfront.net/web/s3_main/Course-home/adobe_illustrator+(1).webp",
+        imgAlt: "",
+        title: "Master GenAI Tools + Frameworks",
+        description:
+          "Learn OpenAI APIs, LangChain, Hugging Face, and more to build intelligent applications effortlessly.",
+      },
+      {
+        imgSrc:
+          "https://d32and0ii3b8oy.cloudfront.net/web/s3_main/Course-home/generated_photos+(1).webp",
+        imgAlt: "",
+        title: "Real-World GenAI Project Portfolio",
+        description:
+          "Work on live projects like chatbot creation, workflow automation, and RAG-powered assistants to showcase your expertise.",
+      },
+      {
+        imgSrc:
+          "https://d32and0ii3b8oy.cloudfront.net/web/s3_main/Course-home/smart_upscaler+(1).webp",
+        imgAlt: "",
+        title: "Higher Earning Potential",
+        description:
+          "Leverage advanced GenAI knowledge to drive business success, enhancing career prospects and salary growth.",
+      },
+    ],
+    bannerImage: {
+      src: "https://d32and0ii3b8oy.cloudfront.net/web/s3_main/Course-home/genAI_DEV+(1).webp  ",
+      width: 350,
+      height: 350,
+      priority: true,
+      quality: 80,
+      alt: "Banner Image 1 Alt",
+    },
+  };
   const DataScienceCourseData = parseJSONData(DataScienceCourseDataJson);
 
   return (
     <>
-       <Head>
-        <title>Advance Gen AI Developer Course for Professionals | Learnbay </title>
+      <Head>
+        <title>
+          Advance Gen AI Developer Course for Professionals | Learnbay{" "}
+        </title>
         <meta
           name="description"
           content="Join Learnbay's Advance Gen AI Developer course. Develop cutting-edge AI solutions and take your career to the next level with hands-on learning."
         />
-      
+
         <meta name="robots" content="index, follow" />
         <meta
           name="keywords"
           content="generative ai course online, generative ai course, generative ai courses, gen ai developer course, gen ai course online, advance gen ai program, gen ai course for professionals, advance gen ai course for professionals, advance generative ai course, generative ai course for professionals"
         />
-        
+
         <link
           rel="icon"
           href="https://d32and0ii3b8oy.cloudfront.net/web/s3_main/cloud-computing/website-icon.webp"
@@ -89,7 +89,7 @@ function Blockchain({ DataScienceCourseDataJson }) {
           rel="canonical"
           href="https://www.learnbay.co/fullstack/generative-ai-course-for-professionals"
         />
-           <meta
+        <meta
           property="og:url"
           content="https://www.learnbay.co/fullstack/generative-ai-course-for-professionals"
         />
@@ -128,20 +128,15 @@ function Blockchain({ DataScienceCourseDataJson }) {
           name="twitter:image"
           content="https://www.learnbay.co/_next/image?url=https%3A%2F%2Flearnbay-wb.s3.ap-south-1.amazonaws.com%2Fmain%2FLearnbay-Logo.webp&w=256&q=100"
         />
-
-
-
       </Head>
       <main>
-        <Navbar interstedInHide={true}  dataScience={true} ADS={true}  />
+        <Navbar interstedInHide={true} dataScience={true} ADS={true} />
         <FirstSection
           interstedInHide={true}
-           dataScience={true}
+          dataScience={true}
           ADS={true}
           noImg={true}
           dataScienceCounselling={true}
-
-  
           radio={true}
           upSkillingHide={true}
           ThirdTyped="Live Interactive Classes"
@@ -158,32 +153,33 @@ function Blockchain({ DataScienceCourseDataJson }) {
           backgroundImage="https://d32and0ii3b8oy.cloudfront.net/web/s3_main/Course-home/SyllabusIcon/iitGen_twos.webp"
           GenAI={true}
         />
-    
+
         <PSummaryAD
-          summaryData={DataScienceCourseData.GenAiDevlopersData[0].summarySection}
+          summaryData={
+            DataScienceCourseData.GenAiDevlopersData[0].summarySection
+          }
         />
         {/* <MasterSecondSection data={seconddata} /> */}
         <JobAbroad
-
           data={JobData}
           dataScience={true}
           interstedInHide={true}
-
           ADS={true}
         />
-                <AnimationGEN         whyChooseData={DataScienceCourseData.GenAiDevlopersData[0].whyChooseData} />
+        <AnimationGEN
+          whyChooseData={
+            DataScienceCourseData.GenAiDevlopersData[0].whyChooseData
+          }
+        />
         <TrainerSection plainimg={true} iimgchmbl={true} />
 
-
         <WhoIsProgram
-        first="Graduation/Masters Degree in CS/IT and MCA."
-        second="2+ year of  work experience is required"
-        third="Mid-career professionals interested in Advance GenAI"
-        forth="Achieving higher career growth in  AI."
-        
-      />
+          first="Graduation/Masters Degree in CS/IT and MCA."
+          second="2+ year of  work experience is required"
+          third="Mid-career professionals interested in Advance GenAI"
+          forth="Achieving higher career growth in  AI."
+        />
         <FullStackSecondSection />
-        
 
         <GetHire />
 
@@ -191,66 +187,62 @@ function Blockchain({ DataScienceCourseDataJson }) {
           noWt={true}
           dataScience={true}
           interstedInHide={true}
-
-
           ADS={true}
         />
         <LJourney />
-        <NewTools toolsData={ DataScienceCourseData.GenAiDevlopersData[0].toolsData} />
+        <NewTools
+          toolsData={DataScienceCourseData.GenAiDevlopersData[0].toolsData}
+        />
 
         <SyllabusNew
           buttonHide={true}
           interstedInHide={true}
-           dataScience={true}
+          dataScience={true}
           ADS={true}
           dataScienceCounselling={true}
-      
           genAIIIT={true}
-
           masterSyllabusMobile={
             DataScienceCourseData.GenAiDevlopersData[0].masterSyllabusMobile
           }
           onebtn={true}
         />
- <Certificate
-          data={
-            DataScienceCourseData.GenAiDevlopersData[0].Certificate
-          }
+        <Certificate
+          data={DataScienceCourseData.GenAiDevlopersData[0].Certificate}
           noTabs={true}
         />
 
         <DSAFeeSection
-            Fee="₹ 95,000"
-            FeeEmi="₹ 6,228/month"
-            weekendbatch="Weekday Evening "
-            weekdaybatch="Weekday Evening "
-            weekday="MON-WED-FRI"
-            weekend="MON-WED-FRI"
-            WeekendDate="JAN 26th"
-            WeekdayDate="JAN 10th "
-            WeekendTime="08:00PM - 10:00PM"
-            WeekdayTime="08:00PM - 10:00PM"
-            FeeContent3="Flexible payment"
-       
-             // <EMI POPUP
-             emiType="NO COST EMI"
-             duration1="18 Months"
-             totalAmount1="₹95,000"
-             monthlyPayment1="₹6,228"
-             greenDown1="Standard Intrest rate Applicable"
-             duration2="12 Months"
-             totalAmount2="₹95,000"
-             monthlyPayment2="₹9,342"
-          />
+          Fee="₹ 1,25,000"
+          FeeEmi="₹ 10,417/month"
+          weekendbatch="Weekday Evening "
+          weekdaybatch="Weekday Evening "
+          weekday="MON-WED-FRI"
+          weekend="MON-WED-FRI"
+          WeekendDate="JAN 26th"
+          WeekdayDate="JAN 10th "
+          WeekendTime="08:00PM - 10:00PM"
+          WeekdayTime="08:00PM - 10:00PM"
+          FeeContent3="Flexible payment"
+          // <EMI POPUP
+          emiType="NO COST EMI"
+          duration1="18 Months"
+          totalAmount1="₹1,25,000"
+          monthlyPayment1="₹7,605"
+          greenDown1="Standard Intrest rate Applicable"
+          duration2="12 Months"
+          totalAmount2="₹1,25,000"
+          monthlyPayment2="₹ 10,417"
+        />
 
         <SliderTabs WithoutService={true} />
-        <GenAIProject  genAiData={
-            DataScienceCourseData.GenAiDevlopersData[0].genAiData
-          }  Duration={true}/>
+        <GenAIProject
+          genAiData={DataScienceCourseData.GenAiDevlopersData[0].genAiData}
+          Duration={true}
+        />
         <SeventhSection />
-        <EightSection interstedInHide={true}  dataScience={true} ADS={true}  />
+        <EightSection interstedInHide={true} dataScience={true} ADS={true} />
         <WhatsappFloat />
-        <BottomBar interstedInHide={true}  dataScience={true} ADS={true}  />
+        <BottomBar interstedInHide={true} dataScience={true} ADS={true} />
       </main>
     </>
   );
