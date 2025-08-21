1bb3e10d3e8b2a413cfc3d4f47b663c2410b70cc
sohel <syed.sohel@learnbay.co>
2025-08-22 00:18:46 +0530

    added two forms appointment booking and career interest inquiry

---
+++ src/pages/career-interest-inquiry.js
@@ -0,0 +1,12 @@
+import Form2 from "@/components/Form/Form-02";
+import React from "react";
+
+const careerInterestInquiry = () => {
+  return (
+    <div>
+      <Form2 />
+    </div>
+  );
+};
+
+export default careerInterestInquiry;
